90881576f76677986efd82ee15789550b4aaed74
Robert Gray <robert.c.gray@gmail.com>
2020-05-21 21:05:09 -0400

    part 2, end of day 2

--- db/index.js
+++ db/index.js
@@ -84,6 +84,20 @@ async function getUserById(userId) {
   }
 }
 
+async function getUserByUsername(username) {
+  try {
+    const { rows: [user] } = await client.query(`
+      SELECT *
+      FROM users
+      WHERE username=$1
+    `, [username]);
+
+    return user;
+  } catch (error) {
+    throw error;
+  }
+}
+
 /**
  * POST Methods
  */
@@ -357,5 +371,6 @@ module.exports = {
   createTags,
   addTagsToPost,
   getPostsByTagName,
-  getAllTags
+  getAllTags,
+  getUserByUsername
 }
