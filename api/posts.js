a67429fa958a74051de3317894f57a7cdc57fdbf
Robert Gray <robert.c.gray@gmail.com>
2020-05-24 18:15:32 -0400

    end of part 2 day 3

--- api/posts.js
+++ api/posts.js
@@ -1,6 +1,35 @@
 const express = require('express');
 const postsRouter = express.Router();
-const { getAllPosts } = require('../db');
+const { 
+  getAllPosts,
+  createPost,
+  updatePost,
+  getPostById
+ } = require('../db');
+const { requireUser } = require('./utils');
+
+postsRouter.post('/', requireUser, async (req, res, next) => {
+  const { title, content, tags = ""} = req.body;
+
+  const tagArr = tags.trim().split(/\s+/)
+  const postData = {};
+  const authorId = req.user.id;
+
+  if (tagArr.length) {
+    postData.tags = tagArr;
+  }
+
+  try {
+    // add authorId, title, content to postData object
+    postData = { authorId, title, content};
+    const post = await createPost(postData);
+    // this will create the post and the tags for us
+    res.send({ post });
+    // otherwise, next an appropriate error object 
+  } catch ({ name, message }) {
+    next({ name, message });
+  }
+});
 
 postsRouter.use((req, res, next) => {
   console.log("A request is being made to /posts");
@@ -16,5 +45,82 @@ postsRouter.get('/', async (req, res) => {
     posts
   });
 });
+
+postsRouter.get('/', async (req, res) => {
+  try {
+    const allPosts = await getAllPosts();
+
+    const posts = allPosts.filter(post => {
+      // keep a post if it is either active, or if it belongs to the current user
+      return post.active || (req.user && post.author.id === req.user.id);
+    });
+
+    res.send({
+      posts
+    });
+  } catch ({ name, message }) {
+    next({ name, message });
+  }
+});
+
+postsRouter.patch('/:postId', requireUser, async (req, res, next) => {
+  const { postId } = req.params;
+  const { title, content, tags } = req.body;
+
+  const updateFields = {};
+
+  if (tags && tags.length > 0) {
+    updateFields.tags = tags.trim().split(/\s+/);
+  }
+
+  if (title) {
+    updateFields.title = title;
+  }
+
+  if (content) {
+    updateFields.content = content;
+  }
+
+  try {
+    const originalPost = await getPostById(postId);
+
+    if (originalPost.author.id === req.user.id) {
+      const updatedPost = await updatePost(postId, updateFields);
+      res.send({ post: updatedPost })
+    } else {
+      next({
+        name: 'UnauthorizedUserError',
+        message: 'You cannot update a post that is not yours'
+      })
+    }
+  } catch ({ name, message }) {
+    next({ name, message });
+  }
+});
+
+postsRouter.delete('/:postId', requireUser, async (req, res, next) => {
+  try {
+    const post = await getPostById(req.params.postId);
+
+    if (post && post.author.id === req.user.id) {
+      const updatedPost = await updatePost(post.id, { active: false });
+
+      res.send({ post: updatedPost });
+    } else {
+      // if there was a post, throw UnauthorizedUserError, otherwise throw PostNotFoundError
+      next(post ? { 
+        name: "UnauthorizedUserError",
+        message: "You cannot delete a post which is not yours"
+      } : {
+        name: "PostNotFoundError",
+        message: "That post does not exist"
+      });
+    }
+
+  } catch ({ name, message }) {
+    next({ name, message })
+  }
+});
+
 
 module.exports = postsRouter;
--- api/tags.js
+++ api/tags.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const tagsRouter = express.Router();
-const { getAllTags } = require('../db');
+const { 
+  getAllTags,
+  getPostsByTagName,
+ } = require('../db');
 
 tagsRouter.use((req, res, next) => {
   console.log("A request is being made to /tags");
@@ -15,6 +18,21 @@ tagsRouter.get('/', async (req, res) => {
   res.send({
     tags
   });
+});
+
+tagsRouter.get('/:tagName/posts', async (req, res, next) => {
+  // read the tagname from the params
+  const tagName = req.name;
+  try {
+    // use our method to get posts by tag name from the db
+    const posts = getPostsByTagName(tagName);
+    // send out an object to the client { posts: // the posts }
+    res.send({
+      posts
+    });
+  } catch ({ name, message }) {
+    // forward the name and message to the error handler
+  }
 });
 
 module.exports = tagsRouter;
